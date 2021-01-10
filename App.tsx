0892070e5615a227d40c8bec8b3da625bc8e4b1d
Touhidul Shawan <touhidulshawan@gmail.com>
2021-01-10 14:58:10 +0600

    add Keyboard dismiss

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import Header from "./components/Header";
 import Todos from "./containers/Todos";
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Header />
-      <View style={styles.content}>
-        <Todos />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View>
+        <Header />
+        <View style={styles.content}>
+          <Todos />
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 export default App;
